160468bd31a30e9db7c324d2c5ee1e167895edbe
Mqseal <30034267@cc.peralta.edu>
2026-05-24 08:26:30 +0000

    Fix player rendering and game loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,25 @@ using namespace std;
 
 int main() {
     Game game;
-
     game.start();
 
-    while (true) {
-        game.update();
+    char move;
+
+    while (!game.isGameOver()) {
         game.display();
+
+        cout << "Move (W/A/S/D, Q to quit): ";
+        cin >> move;
+
+        if (move == 'q' || move == 'Q') {
+            break;
+        }
+
+        game.movePlayer(move);
+        game.update();
     }
+
+    cout << "Game over!" << endl;
 
     return 0;
 }
